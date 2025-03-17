f2fbc1c7b9d4c4fa5404cccdb207dda52b8720de
unknown <noblefedlu@gmail.com>
2025-03-17 13:01:38 +0300

    er oooo

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axioc from 'axios';
 
 const axiosInstance = axioc.create({
-  baseURL: 'http://127.0.0.1:5001/lone-2025/us-central1/api'
+  // baseURL: 'http://127.0.0.1:5001/lone-2025/us-central1/api',
+  baseURL: 'https://amazon-api-deploy-5gba.onrender.com/'
 });
 
 export {axiosInstance};
